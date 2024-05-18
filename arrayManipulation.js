c01605fc12ee76743f607407bbc5037080936cee
Noble054 <atetteh325@st.ug.edu.gh>
2024-05-18 12:23:19 +0000

    Add formatArrayStrings function

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -3,5 +3,12 @@
 function processArray(numbers) {
     return numbers.map(num => num % 2 === 0 ? num ** 2 : num * 3);
 }
+function formatArrayStrings(strings, numbers) {
+    return strings.map((str, index) => {
+        return numbers[index] % 2 === 0 ? str.toUpperCase() : str.toLowerCase();
+    });
+}
 
-module.exports = { processArray };
+
+
+module.exports = { processArray, formatArrayStrings };
